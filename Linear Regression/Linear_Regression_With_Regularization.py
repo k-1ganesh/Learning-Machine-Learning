9ff9d0d4d1ac249d43defd97f6ef0614f7007b4b
Ganesh <kaganesh12@gmail.com>
2024-09-21 22:17:43 +0530

    Learned and Implemented Regularization for Regression

---
+++ Linear Regression/Linear_Regression_With_Regularization.py
@@ -0,0 +1,38 @@
+import numpy as np
+
+def Predict(x , w , b): # In this x is 1D array
+    return np.dot(x , w) + b
+
+
+def Gradient(x , y , w , b,lamda): # In this case x is 2D array
+    m = x.shape[0]
+    d_w = np.zeros(x.shape[1])
+    d_b = 0
+    for i in range(m):
+        for j in range(x.shape[1]):
+            d_w[j] += (np.dot(w , x[i]) + b - y[i])*x[i][j]
+        d_b += (np.dot(w , x[i]) + b - y[i])
+    d_w = d_w / m
+    d_b = d_b / m
+    d_w += (lamda*w / m)
+    return d_w , d_b
+
+def Gradient_Descent(x , y , w , b , alpha): # In this case x is 2D array
+    iter = 10000
+    lamda = 10
+    for i in range(iter):
+        d_w , d_b = Gradient(x , y , w , b,lamda)
+        w = w - alpha * d_w
+        b = b - alpha * d_b
+    return w , b
+
+x = np.array([[2104, 5, 1, 45], [1416, 3, 2, 40], [852, 2, 1, 35]]) 
+y = np.array([460, 232, 178])  
+w , b = Gradient_Descent(x,y,np.zeros(4),0,1.0e-8)
+input = np.array( [2104,5,1,45])
+print(Predict(input , w , b))
+
+
+
+
+
